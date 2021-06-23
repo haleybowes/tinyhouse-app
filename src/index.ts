4a486b98f1680635dbf1b1837688f0dee3f6719a
Haley Bowes <haley.bowes@loblaw.ca>
2021-06-23 14:09:56 -0400

    updating seeds/resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { typeDefs, resolvers } from './graphql';
+import { typeDefs } from './graphql';
+import { connectDatabase } from './database'; 
+import { resolvers } from './graphql/resolvers';
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
-server.applyMiddleware({ app, path: '/api' })
-app.use(express.json());
-const port = 9000;
 
-app.listen(port);
+const mount = async (app: Application) => {
+    const db = await connectDatabase();
+    const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) });
+    server.applyMiddleware({ app, path: '/api' })
+    app.use(express.json());
+    app.listen(process.env.PORT);
 
-console.log(`[app]: http://localhost:${port}`);
+    console.log(`[app]: http://localhost:${process.env.PORT}`);
+
+    // const listings = await db.listings.find({}).toArray();
+    // console.log(listings);
+}
+
+mount(express());
